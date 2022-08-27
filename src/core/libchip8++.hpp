bf2fa0bd5fa5dd1041aa122ea9f56f633ddd0c2a
icebarf <sysgrammer@protonmail.com>
2022-08-27 17:59:32 +0530

    document the library - doxygen

--- src/core/libchip8++.hpp
+++ src/core/libchip8++.hpp
@@ -27,29 +27,46 @@
 #include <random>
 #include <sys/types.h>
 
+/**
+ * The main namsepace under which the whole implementation
+ * for the chip8 core system is provided. It contains several
+ * enumerations, a single system class, a few utility functions
+ * for the implementation.
+ */
 namespace Chip8_core {
 
 namespace fs = std::filesystem;
 
+/**
+ * Defines several constants used throughout
+ * the implementation of instructions.
+ */
 enum Constants {
-    MEMSIZE = 4096,
-    DISPW = 64,
-    DISPH = 32,
-    STACKSIZE = 48,
-    REGCNT = 16,
-    PROGRAM_LD_ADDR = 0x200,
-    INIT_STACK_TOP = -1,
-    KEYCOUNT = 16
+    MEMSIZE = 4096,          /**< The Chip8 internal memory size. */
+    DISPW = 64,              /**< The Chip8 internal display width. */
+    DISPH = 32,              /**< The Chip8 internal display height. */
+    STACKSIZE = 48,          /**< The Chip8 internal stack height.  */
+    REGCNT = 16,             /**< The Chip8 internal register count. */
+    PROGRAM_LD_ADDR = 0x200, /**< The address at which Chip8 ROMs are loaded. */
+    INIT_STACK_TOP = -1,     /**< The default value for stack top. */
+    KEYCOUNT = 16            /**< The number of keys present in Chip8. */
 };
 
 // clang-format off
+
+/**
+ * Contains named constants for Chip8 Keys.
+ */
 enum KeyCode {
-    One,  Two, Three, Four,
-    Five, Six, Seven, Eight,
-    Nine, Ten, A,     B,
-    C,    D,   E,     F
+    Zero,  One,  Two, Three,
+    Four,  Five, Six, Seven,
+    Eight, Nine, A,   B,
+    C,     D,    E,   F
 };
 
+/**
+ * Contains named constants for Chip8 Registers.
+ */
 enum Registers {
     R0, R1, R2, R3,
     R4, R5, R6, R7,
@@ -58,14 +75,31 @@ enum Registers {
 };
 // clang-format on
 
+/**
+ * Contains named constants for various Chip8 instruction quriks.
+ */
 enum Quirks {
-    MATT,           // shift VY by 1 store in VX
-    COWGOD,         // shift VX by 1 store in VX
-    LOAD_INDEX_REG, // set index register to I + X + 1
+    MATT,           /**< From Mattmikolay's documentation. Shift RY by 1, store in RX. */
+    COWGOD,         /**< From Cowgod's Chip8 Reference. Shift RX by 1, store in RX. */
+    LOAD_INDEX_REG, /**< From Mattmikolay's documentation. Set index register to I + X + 1. */
 };
 
-enum Key { UP, DOWN };
+/**
+ * Contains named constants for representing pressed and
+ * not pressed keystate. Usually used internally maybe used in the frontend.
+ */
+enum Key {
+    UP,  /**< The key is not pressed. */
+    DOWN /**< The key is pressed. */
+};
 
+/**
+ * Represents the entire Chip8 internal state.
+ * It provides access to private data through pairs of Getters and Setters.
+ * There are some methods that allow access to internal data via Reference. These should not be
+ * used anywhere in the frontend as it can mess with the state. These exist only to ease the
+ * implementation of instructions internally.
+ */
 class system {
   private:
     std::array<uint8_t, Constants::MEMSIZE> memory;
@@ -82,15 +116,23 @@ class system {
     std::uniform_int_distribution<int> distb;
 
   public:
+    /**
+     * Constructs the Chip8 class.
+     * This constructor loads the font into memory at address 0x0, sets the program_counter to
+     * 0x200 which is the program load address, sets the stack top, initialises random number
+     * generation engine with device argument, sets the distribution between 0 and 255
+     * (inclusive) and zero initialises the rest.
+     * @param device rvalue reference to a std::random_device for generating random numbers.
+     * @see Constants
+     */
     system(std::random_device&& device)
-      : memory{ 0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70,
-                0xF0, 0x10, 0xF0, 0x80, 0xF0, 0xF0, 0x10, 0xF0, 0x10, 0xF0,
-                0x90, 0x90, 0xF0, 0x10, 0x10, 0xF0, 0x80, 0xF0, 0x10, 0xF0,
-                0xF0, 0x80, 0xF0, 0x90, 0xF0, 0xF0, 0x10, 0x20, 0x40, 0x40,
-                0xF0, 0x90, 0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0, 0x10, 0xF0,
-                0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0,
-                0xF0, 0x80, 0x80, 0x80, 0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0,
-                0xF0, 0x80, 0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80 }
+      : memory{ 0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xF0, 0x10,
+                0xF0, 0x80, 0xF0, 0xF0, 0x10, 0xF0, 0x10, 0xF0, 0x90, 0x90, 0xF0, 0x10,
+                0x10, 0xF0, 0x80, 0xF0, 0x10, 0xF0, 0xF0, 0x80, 0xF0, 0x90, 0xF0, 0xF0,
+                0x10, 0x20, 0x40, 0x40, 0xF0, 0x90, 0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0,
+                0x10, 0xF0, 0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0,
+                0xF0, 0x80, 0x80, 0x80, 0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0, 0xF0, 0x80,
+                0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80 }
       , display{ 0 }
       , stack{ 0 }
       , registers{ 0 }
@@ -104,6 +146,11 @@ class system {
     {
     }
 
+    /**
+     * Loads the rom into memory at address 0x200 which is the program load address.
+     * @param rom path to the rom file, use like this Chip8.LoadRom("games/chip8_rom.ch8")
+     * @see Constants
+     */
     void LoadRom(fs::path rom)
     {
         assert(fs::exists(rom));
@@ -117,134 +164,237 @@ class system {
         assert(rs.gcount() == size);
     }
 
+    /**
+     * Fetches an 16-bit opcode from memory. Also increments the program_counter by 2.
+     * @return 16-bit opcode containing a full chip8 instruction
+     */
     uint16_t Fetch()
     {
-        uint16_t opcode =
-          (memory[program_counter] << 8) | memory[program_counter + 1];
+        uint16_t opcode = (memory[program_counter] << 8) | memory[program_counter + 1];
         program_counter += 2;
         return opcode;
     }
 
+    /**
+     * Returns a reference to private data member memory.
+     * @return reference to chip8 memory
+     */
     std::array<uint8_t, Constants::MEMSIZE>& RefMemory()
     {
         return memory;
     }
 
+    /**
+     * Sets a register to some value.
+     * @param r the register to modify, like SetRegister(0xA, 0x288) or
+     * SetRegister(Registers::RA, 0x288)
+     * @param v the value to set the register to.
+     */
     void SetRegister(Registers r, uint8_t v)
     {
         registers[r] = v;
     }
 
+    /**
+     * Returns the value stored in a register.
+     * @param r the register to get the value of, like GetRegitser(0xA) or
+     * GetRegister(Registers::RA)
+     */
     uint8_t GetRegister(Registers r)
     {
         return registers[r];
     }
 
+    /**
+     * Returns a reference to private data member registers.
+     * @return reference to chip8 register array
+     */
     std::array<uint8_t, Constants::REGCNT>& RefRegisterArray()
     {
         return registers;
     }
 
+    /**
+     * Set the value of the index register.
+     * @param v the value to set
+     */
     void SetIndexRegister(uint16_t v)
     {
         index_reg = v;
     }
 
+    /**
+     * Returns the value stored in index register.
+     * @return 16-bit value. Probably an address
+     */
     uint16_t GetIndexRegister()
     {
         return index_reg;
     }
 
+    /**
+     * Set a key to a UP or DOWN.
+     * @param k KeyCode representing the key to set
+     * @param v A value containing either Key::UP or Key::DOWN
+     */
     void SetKey(KeyCode k, uint8_t v)
     {
         keys[k] = v;
     }
 
+    /**
+     * Returns the value to which a key is set.
+     * @param k KeyCode representing the key
+     * @return either Key::UP or Key::DOWN
+     */
     uint8_t GetKey(KeyCode k)
     {
         return keys[k];
     }
 
+    /**
+     * Push an address to the stack. Also increment the stacktop
+     * @param v the address to push
+     */
     void Push(uint16_t v)
     {
         stack[++stacktop] = v;
     }
 
+    /**
+     * Pop off a value from the top of the stack and return it. Also decrement the stacktop
+     * @return A 16-bit value containig the address from top
+     */
     uint16_t Pop()
     {
         return stack[stacktop--];
     }
 
+    /**
+     * Set the program_counter to some address.
+     * @param v the address to set
+     */
     void SetPC(uint16_t v)
     {
         program_counter = v;
     }
 
+    /**
+     * Returns the current value of program_counter.
+     * @return A 16-bit address
+     */
     uint16_t GetPC()
     {
         return program_counter;
     }
 
+    /**
+     * Increment the program_counter by 1.
+     */
     void IncPC()
     {
         program_counter++;
     }
 
+    /**
+     * Decrement the program_counter by 1.
+     */
     void DecPC()
     {
         program_counter--;
     }
 
+    /**
+     * Set the delay_timer to some value.
+     * @param v the value to set
+     */
     void SetDT(uint8_t v)
     {
         delay_timer = v;
     }
 
+    /**
+     * Get the current value of delay_timer.
+     * @return value of the delay timer
+     */
     uint8_t GetDT()
     {
         return delay_timer;
     }
 
+    /**
+     * Increment the delay timer by 1
+     */
     void IncDT()
     {
         delay_timer++;
     }
 
+    /**
+     * Decrement the delay timer by 1
+     */
     void DecDT()
     {
         delay_timer--;
     }
 
+    /**
+     * Set the sound_timer to some value.
+     * @param v the value to set
+     */
     void SetST(uint8_t v)
     {
         sound_timer = v;
     }
 
+    /**
+     * Get the current value of sound_timer.
+     * @return value of the sound timer
+     */
     uint8_t GetST()
     {
         return sound_timer;
     }
 
+    /**
+     * Increment the sound timer by 1
+     */
     void IncST()
     {
         sound_timer++;
     }
 
+    /**
+     * Decrement the sound timer by 1
+     */
     void DecST()
     {
         sound_timer--;
     }
 
+    /**
+     * Set a pixel in the display array to either 1 or 0.
+     * @param idx the index at which the pixel resides, computed as (column + (row * 64))
+     * @param v the 1 or 0 value to set
+     */
     void SetPixel(uint16_t idx, uint8_t v)
     {
         display[idx] = v;
     }
 
+    /**
+     * Get the value of a pixel in the display array.
+     * @param idx the index at which the pixel resides, computed as (column + (row * 64))
+     * @return either 1 or 0 representing SET or UNSET respectively
+     */
     uint8_t GetPixel(uint16_t idx)
     {
         return display[idx];
     }
 
+    /**
+     * Subscript operator overload allowing access to memory array of Chip8 class.
+     */
     uint8_t& operator[](long i)
     {
         assert(i > 0);
@@ -253,37 +403,38 @@ class system {
         return memory[i];
     }
 
+    /**
+     * Reset the entire display i.e set all pixels to 0 (UNSET)
+     */
     void reset_display()
     {
         display.reset();
     }
 
+    /**
+     * Reset all the keys i.e set all keys to Key::UP (un-pressed)
+     */
     void reset_keys()
     {
         keys.reset();
     }
 
+    /**
+     *  Returns a random integer between 0 and 255.
+     * @return unsigned 8 bit random integer
+     */
     uint8_t InternalRand()
     {
         return distb(engine);
     }
 };
 
-/* excatly what the function names denote
- * let a 16-bit number be denoted as
- * XXXX YYYY ZZZZ AAAA
- * then X - Y makes up the higher byte of the opcode
- * and  Z - A makes up the lower byte of the opcode*/
-uint8_t
-fetch_nib1(uint16_t opcode);
-uint8_t
-fetch_nib2(uint16_t opcode);
-uint8_t
-fetch_nib3(uint16_t opcode);
-uint8_t
-fetch_nib4(uint16_t opcode);
+/**
+ * Subnamespace inside the Chip8_core namespace.
+ * Contains all declarations and definitions of Chip8 instructions.
+ */
+namespace Instructions {
 
-/* Instructions */
 void
 sys_addr(uint16_t opcode, system Chip8);
 void
@@ -359,6 +510,11 @@ load_memory_into_reg(Quirks q, uint16_t opcode, system Chip8);
 #undef LIBCHIP8_IMPLEMENTATION_SOURCE
 
 /* utility functions */
+/* excatly what the function names denote
+ * let a 16-bit number be denoted as
+ * XXXX YYYY ZZZZ AAAA
+ * then X - Y makes up the higher byte of the opcode
+ * and  Z - A makes up the lower byte of the opcode*/
 inline uint8_t
 nibble2byte(uint8_t un, uint8_t ln) // NOLINT(misc-definitions-in-headers)
 {
@@ -458,9 +614,8 @@ void
 add(uint16_t opcode, system Chip8) // NOLINT(misc-definitions-in-headers)
 {
     auto rx = static_cast<Registers>(fetch_nib2(opcode));
-    Chip8.SetRegister(rx,
-                      Chip8.GetRegister(rx) +
-                        nibble2byte(fetch_nib3(opcode), fetch_nib4(opcode)));
+    Chip8.SetRegister(
+      rx, Chip8.GetRegister(rx) + nibble2byte(fetch_nib3(opcode), fetch_nib4(opcode)));
 }
 
 void
@@ -515,8 +670,7 @@ rsubc(uint16_t opcode, system Chip8) // NOLINT(misc-definitions-in-headers)
     auto ry = static_cast<Registers>(fetch_nib3(opcode));
     Chip8.SetRegister(Registers::RF, 0);
 
-    if (Chip8.GetRegister(rx) > Chip8.GetRegister(ry))
-        Chip8.SetRegister(Registers::RF, 1);
+    if (Chip8.GetRegister(rx) > Chip8.GetRegister(ry)) Chip8.SetRegister(Registers::RF, 1);
 
     Chip8.SetRegister(rx, Chip8.GetRegister(rx) - Chip8.GetRegister(ry));
 }
@@ -555,8 +709,7 @@ rsubc_reverse(uint16_t opcode, system Chip8) // NOLINT(misc-definitions-in-heade
     auto ry = static_cast<Registers>(fetch_nib3(opcode));
     Chip8.SetRegister(Registers::RF, 0);
 
-    if (Chip8.GetRegister(rx) < Chip8.GetRegister(ry))
-        Chip8.SetRegister(Registers::RF, 1);
+    if (Chip8.GetRegister(rx) < Chip8.GetRegister(ry)) Chip8.SetRegister(Registers::RF, 1);
 
     Chip8.SetRegister(rx, Chip8.GetRegister(ry) - Chip8.GetRegister(rx));
 }
@@ -571,8 +724,7 @@ rshift_left(Quirks mode, // NOLINT(misc-definitions-in-headers)
     Chip8.SetRegister(Registers::RF, 0);
 
     auto shift = [&](Registers to_shift, uint8_t shift_cnt) {
-        if (Chip8.GetRegister(to_shift) & 0b1000'0000)
-            Chip8.SetRegister(Registers::RF, 1);
+        if (Chip8.GetRegister(to_shift) & 0b1000'0000) Chip8.SetRegister(Registers::RF, 1);
         Chip8.SetRegister(rx, Chip8.GetRegister(to_shift) << shift_cnt);
     };
 
@@ -617,19 +769,16 @@ void
 genrandom(uint16_t opcode, system Chip8) // NOLINT(misc-definitions-in-headers)
 {
     auto rx = static_cast<Registers>(fetch_nib2(opcode));
-    Chip8.SetRegister(rx,
-                      Chip8.InternalRand() &
-                        nibble2byte(fetch_nib3(opcode), fetch_nib4(opcode)));
+    Chip8.SetRegister(
+      rx, Chip8.InternalRand() & nibble2byte(fetch_nib3(opcode), fetch_nib4(opcode)));
 }
 
 void
 draw(uint16_t opcode, system Chip8) // NOLINT(misc-definitions-in-headers)
 {
     /* tobias vl's dxyn impl*/
-    uint8_t val_x =
-      Chip8.GetRegister(static_cast<Registers>(fetch_nib2(opcode))) & 63;
-    uint8_t val_y =
-      Chip8.GetRegister(static_cast<Registers>(fetch_nib3(opcode))) & 31;
+    uint8_t val_x = Chip8.GetRegister(static_cast<Registers>(fetch_nib2(opcode))) & 63;
+    uint8_t val_y = Chip8.GetRegister(static_cast<Registers>(fetch_nib3(opcode))) & 31;
     uint8_t N = fetch_nib4(opcode);
 
     Chip8.SetRegister(Registers::RF, 0);
@@ -670,8 +819,7 @@ skip_ifkeynotpress(uint16_t opcode, // NOLINT(misc-definitions-in-headers)
                    system Chip8)
 {
     auto regval = Chip8.GetRegister(static_cast<Registers>(fetch_nib2(opcode)));
-    if (Chip8.GetKey(static_cast<KeyCode>(regval)) == Key::UP)
-        Chip8.SetPC(Chip8.GetPC() + 2);
+    if (Chip8.GetKey(static_cast<KeyCode>(regval)) == Key::UP) Chip8.SetPC(Chip8.GetPC() + 2);
 }
 
 void
@@ -761,7 +909,7 @@ load_memory_into_reg(Quirks q, // NOLINT(misc-definitions-in-headers)
 }
 
 #endif
-
+} // namespace Instructions
 } // namespace Chip8_core
 
 #endif
